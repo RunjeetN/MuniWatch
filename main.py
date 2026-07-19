ff5f50acad7e929ca0009f290f9ff390000580bd
Runjeet Narula <runjeetnarula@berkeley.edu>
2026-07-19 13:36:23 -0700

    define calc_minutes_to_arrival function

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+import requests 
+import json
+from datetime import datetime, timezone
+from dateutil.parser import isoparse
+from math import floor
+from collections import defaultdict
+
 # setup API
 api_token = "fe08bdf9-b21c-42d4-ad23-603f2ef284ef"
 agency_id = "SF"
 eastbound_stopcode = 15419
 westbound_stopcode = 16996
 
-import requests 
-import json
-
 
 response = requests.get(f'http://api.511.org/transit/StopMonitoring?api_key={api_token}&agency={agency_id}&stopcode={eastbound_stopcode}&format=json')
 
-
+def calc_minutes_to_arrival(arrival_time: str) -> int:
+    # input: 2026-07-19T20:14:34Z
+    dt = isoparse(arrival_time)
+    curr_time = datetime.now(timezone.utc)
+    diff = floor(((dt - curr_time).total_seconds()) / 60)
+    return diff
 
 def getLineInfo(stopcode: int) -> None:
-    res = []
+    res = defaultdict(list)
     data = json.loads(response.content.decode('utf-8-sig'))
 
     lines = data["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"]
@@ -21,9 +30,9 @@ def getLineInfo(stopcode: int) -> None:
         journeyInfo = entry["MonitoredVehicleJourney"]
         expected_arrival_time = entry["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"]
         line_name = journeyInfo["LineRef"]
-        res.append([line_name, expected_arrival_time])
+        res[line_name].append(calc_minutes_to_arrival(expected_arrival_time))
     return res 
-    #print(json.dumps(line, indent=4))
+    
 
 
 east_bound_info = getLineInfo(eastbound_stopcode)
